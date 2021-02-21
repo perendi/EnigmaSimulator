1280e0279f286f0f0c966f76e3b8996067e91a62
Daniel Matyas Perendi <dmperendi1@sheffield.ac.uk>
2021-02-21 15:03:29 +0000

    New rotors and reflectors generated

--- src/enigma/Reflector.java
+++ src/enigma/Reflector.java
@@ -9,11 +9,11 @@ public class Reflector {
 
 	// M3 default reflector
 	static final String REFLECTOR_B = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
-	static final String REFLECTOR_HUN = ""; //TODO
+	static final String REFLECTOR_HUN = "RÓÉMWTBYJXOÚGLKCŐIÁZNSÜAPEŰÍQUVDHFÖ"; //V is not reflected
 
 	// M4 default reflector
 	static final String REFLECTOR_B_THIN = "ENKQAUYWJICOPBLMDXZVFTHRGS";
-	static final String REFLECTOR_HUN_THIN = ""; //TODO
+	static final String REFLECTOR_HUN_THIN = "NZVŐÓKJWSIHMÉEÚÍAÖDOCYTXGQÜLUŰBFRPÁ"; //Ű is not reflected
 
 	/**
 	 * Simulates the reflection for the M3 machine
@@ -24,7 +24,7 @@ public class Reflector {
 	public static int reflect_M3(int index, Alphabet a) {
 		if(a.name.equals("Hungarian")){
 			// Return hungarian reflection
-			return 0;
+			return Rotor.toIndex(REFLECTOR_HUN.charAt(index));
 		}
 		else{
 			return Rotor.toIndex(REFLECTOR_B.charAt(index));
@@ -41,7 +41,7 @@ public class Reflector {
 	public static int reflect_M4(int index, Alphabet a) {
 		if(a.name.equals("Hungarian")){
 			// Return hungarian reflection
-			return 0;
+			return Rotor.toIndex(REFLECTOR_HUN_THIN.charAt(index));
 		}
 		else{
 			return Rotor.toIndex(REFLECTOR_B_THIN.charAt(index));
